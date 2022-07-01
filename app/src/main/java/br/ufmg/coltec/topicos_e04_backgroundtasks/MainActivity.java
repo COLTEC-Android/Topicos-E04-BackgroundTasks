4891bce4287791fcab06f588462494a0fa7ca558
Samuel <samuel@gmail.com>
2022-06-30 21:53:56 -0300

    Topicos-E04-BackgroundTasks - Visualizador de Imagens - 03 - Exibição da barra de progresso

--- app/src/main/java/br/ufmg/coltec/topicos_e04_backgroundtasks/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/topicos_e04_backgroundtasks/MainActivity.java
@@ -1,4 +1,6 @@
 package br.ufmg.coltec.topicos_e04_backgroundtasks;
+import static android.view.View.VISIBLE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
@@ -8,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+
 
 import java.io.IOException;
 
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         Button downloadBtn = findViewById(R.id.btn_download);
         EditText txtLink = findViewById(R.id.txt_img_link);
         ImageView imgView = findViewById(R.id.img_picture);
+        ProgressBar bar = findViewById(R.id.barra);
 
 //        TODO[1]: Processo de download e carregamento da imagem acontecendo na Main Thread, ALTERAR!!
         downloadBtn.setOnClickListener(new View.OnClickListener() {
@@ -28,16 +33,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO[2]: Exibir barra de progresso quando estiver fazendo download da imagem
-
+                bar.setVisibility(VISIBLE);
                 Thread t = new Thread(){
                     public void run(){
                         //thread "secundaria"
                         Bitmap img = MainActivity.this.downloadImage(txtLink.getText().toString());
+
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
                                 //thread "principal"
+                                bar.setVisibility(View.INVISIBLE);
                                 imgView.setImageBitmap(img);
+
+
                             }
                         });
                     }
